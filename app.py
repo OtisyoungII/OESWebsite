d7e0b576c077f6ef7d48c34a5db055824042d82e
Otis YII <oyoung21@gmail.com>
2026-08-02 23:42:26 -0400

    Add public privacy policy page and links

--- app.py
+++ app.py
@@ -23,5 +23,10 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
